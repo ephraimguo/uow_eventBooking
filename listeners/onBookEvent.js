715f6bcdfeda0c4b970da0cbb8e56aed4d3f7149
ephraimguo <ephraimguo@gmail.com>
2018-11-07 14:02:06 +0800

    added booking email notification

--- listeners/onBookEvent.js
+++ listeners/onBookEvent.js
@@ -1,10 +1,21 @@
 const {sendEmail} = require('../entries/mailer/mailerConfig');
+const moment = require('moment');
 
 module.exports = function({domain}) {
   domain.on({actorType:'UserEventManager', type: 'bookEvent'}, async function(event){
     const eventId = event.data.eventId;
     const adminActorId = event.data.adminActorId;
     const eventActor = await domain.get('Event', eventId);
+
+    const adminActor = await domain.get('User', adminActorId);
+
+    sendEmail(adminActor.data.perEmail, 'Event Booked Successfully',
+      `
+      <h3>${eventActor.data.title}</h3>
+      <p>Time: ${moment(eventActor.data.startTime).format('DD-MM-YYYY HH:mm')}</p>
+      <p>Registered Count: ${eventActor.data.seatTaken}</p>
+      `
+    );
 
     try{
       await eventActor.updateSeats(1, adminActorId);
