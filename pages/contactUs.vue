b3808020893f4dc74f3dd2eb4a9b26bf3b5aca1f
ephraimguo <ephraimguo@gmail.com>
2018-11-08 21:43:02 +0800

    update

--- pages/contactUs.vue
+++ pages/contactUs.vue
@@ -14,6 +14,36 @@
           </div>
         </CarouselItem>
     </Carousel>
+
+    <Row>
+      <Col  span="20" offset="2">
+        <Card>
+          <a href="http://www.sim.edu.sg/Discover-SIM/Documents/SIM-Campuses-Map.pdf"> Download map link </a>
+          <p>IT department: Support Services</p>
+          <p>Phone:+65 6248 9746</p>
+          <p>Email: uow.events@sim.edu.sg</p>
+          <p>Working Hours: 09:00 - 18:00</p>
+          <p>Address : 461 Clementi Road, Singapore 599491</p>
+
+
+          <p>Directions:</p>
+          <p>Buses:</p>
+          <p>52, 61, 74, 75, 151,
+            154, 184</p>
+
+          <p>Nearest MRT stations:</p>
+          <p>Clementi, Dover,</p>
+          <p>King Albert Park</p>
+
+        </Card>
+
+      </Col>
+    </Row>
+
+    <br>
+    <br>
+    <br>
+    <br>
   </section>
 </template>
 
