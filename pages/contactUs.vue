382a91affbf5311c6d5f97920192363be13a7e61
ephraimguo <ephraimguo@gmail.com>
2018-11-08 21:33:11 +0800

    contact us page done

--- pages/contactUs.vue
+++ pages/contactUs.vue
@@ -1,12 +1,42 @@
 <template>
   <section>
-    this is contact us page
+    <Divider orientation="left">Contact Us</Divider>
+
+    <Carousel v-model="carouselDefault">
+        <CarouselItem >
+          <div :style="carouStyle">
+            <img src="../static/uowMap.png" alt="" height="900">
+          </div>
+        </CarouselItem>
+        <CarouselItem >
+          <div :style="carouStyle">
+            <img src="../static/uowPost.jpg" alt="" height="900">
+          </div>
+        </CarouselItem>
+    </Carousel>
   </section>
 </template>
 
 <script>
   export default {
-    name: "contactUs"
+    name: "contactUs",
+    data() {
+      return {
+        carouselDefault: 0,
+        carouStyle: {
+          autoplay: false,
+          autoplaySpeed: 2000,
+          dots: 'inside',
+          radiusDot: false,
+          trigger: 'click',
+          arrow: 'hover',
+          // "width": "100%",
+          "margin":"auto",
+          "border": "2px solid #333",
+          "min-height": "500px"
+      }
+      }
+    }
   }
 </script>
 
